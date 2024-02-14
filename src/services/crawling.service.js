f10eb5141662cebf50a3670923115e2aa775b185
Shivendra singh <sonu@Shivendras-MacBook-Air.local>
2024-02-14 21:17:52 +0530

    add multi url support

--- src/services/crawling.service.js
+++ src/services/crawling.service.js
@@ -3,52 +3,8 @@ const axios = require("axios");
 const puppeteer = require("puppeteer");
 
 const createCrawling = async (bodyData) => {
-  // try {
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
-  //   var imageData = [];
-    
-  //   for (const items of bodyData) {
-  //     try {
-  //       console.log(items.link);
-  //       await page.goto(items.link, {
-  //         waitUntil: "networkidle2",
-  //       });
-    
-  //       const imageSources = await page.evaluate(() => {
-  //         const pictureSelector = "picture";
-  //         const pictureElement = document.querySelector(pictureSelector);
-    
-  //         if (pictureElement) {
-  //           const imgElement = pictureElement.querySelector("img");
-  //           return imgElement ? imgElement.src : null;
-  //         }
-  //         return null;
-  //       });
-  //       if (imageSources !== null) {
-  //         imageData.push({
-  //           url: items.link,
-  //           imageSources: imageSources,
-  //         });
-  //         console.log("Image Sources:", imageSources);
-  //       } else {
-  //         console.log("No image sources found for:", items.link);
-  //       }
-  //       console.log("Image Sources:", imageSources);
-  //     } catch (error) {
-  //       console.error("Error during navigation:", error.message);
-  //       // Handle the error as needed (e.g., continue with the next iteration)
-  //     }
-  //   }
-    
-  //   // Close the browser when done
-  //   await browser.close();
-  //   return imageData;
-  //   // Now, imageData array contains the collected data
-
-  // } catch (error) {
-  //   console.error("Error fetching data:", error);
-  // }
+  console.log(bodyData);
+  
   let imageData = [];
 
   while (true) {
@@ -57,45 +13,83 @@ const createCrawling = async (bodyData) => {
 
     try {
       for (const items of bodyData) {
-        await page.goto(items.link, {
-          waitUntil: 'networkidle2',
-        });
-
-        const imageSources = await page.evaluate(() => {
-          const pictureSelector = 'picture';
-          const pictureElement = document.querySelector(pictureSelector);
-
-          if (pictureElement) {
-            const imgElement = pictureElement.querySelector('img');
-            return imgElement ? imgElement.src : null;
-          }
-          return null;
-        });
-
-        if (imageSources !== null) {
-          imageData.push({
-            url: items.link,
-            imageSources: imageSources,
+        if (validateURL(items.link)) {
+          console.log(`Processing URL: ${items.link}`);
+          await page.goto(items.link, {
+            waitUntil: 'networkidle2',
+            timeout: 0,
           });
-          // console.log('Image Sources:', imageSources);
-        } else {
-          // console.log('No image sources found for:', items.link);
-          // If image source is null, break the loop and retry
-          await browser.close();
-          break;
+  
+          const imageSources = await page.evaluate((items) => {
+            let pictureSelector;
+            if (items.link.includes("mercari")) {
+              console.log(`Processing URL: ${items.link} for jp.mercari.com`);
+              pictureSelector = 'picture';
+            } else if (items.link.includes("amiami")) {
+              console.log(`Processing URL: ${items.link} for www.amiami.jp`);
+              pictureSelector = '.main_image_area';
+            } else if (items.link.includes("rakuten")) {
+              console.log(`Processing URL: ${items.link} for biccamera.rakuten.co.jp`);
+              pictureSelector = '.popup-modal2';
+            } else if (items.link.includes("fril")) {
+              console.log(`Processing URL: ${items.link} for item.fril.jp`);
+              pictureSelector = '#photoFrame';
+            } else if (items.link.includes("paypayfleamarket")) {
+              pictureSelector = '.slick-current';
+            } else {
+              console.log(`No match found for URL: ${items.link}`);
+
+            }
+            const pictureElement = document.querySelector(pictureSelector);
+  
+            if (pictureElement) {
+              const imgElement = pictureElement.querySelector('img');
+              return imgElement ? imgElement.src : null;
+            }
+          }, items);
+          if (imageSources !== null) {
+            imageData.push({
+              url: items.link,
+              imageSources: imageSources,
+            });
+          } else {
+            await browser.close();
+            break;
+          }
         }
       }
-
-      // Close the browser when the loop completes successfully
       await browser.close();
+      console.log(imageData);
       return imageData;
     } catch (error) {
       console.error('Error during navigation:', error.message);
-      // Handle the error as needed
       await browser.close();
     }
   }
 };
+
+function validateURL(url) {
+  const validStrings = [
+    /jp.mercari.com/,
+    /www.amiami.jp/,
+    /biccamera.rakuten.co.jp/,
+    /item.fril.jp/,
+    /paypayfleamarket.yahoo.co.jp/
+  ];
+
+  // Iterate through each pattern
+  for (const pattern of validStrings) {
+    // Check if the URL matches the pattern
+    if (pattern.test(url)) {
+      return true; // Return true if a match is found
+    }
+  }
+
+  // If no match is found after checking all patterns, return false
+  console.log('invalid:', url);
+  return false;
+}
+
 
 module.exports = {
   createCrawling,
