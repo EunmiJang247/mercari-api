2c40a69aa3688f86b25396ee1643f15141f56e22
softforyou <softforyou247@gmail.com>
2024-02-18 23:38:18 +0900

    timeout-add

--- src/services/crawling.service.js
+++ src/services/crawling.service.js
@@ -5,32 +5,25 @@ const createCrawling = async (bodyData) => {
   const page = await browser.newPage();
   
   let imageData = [];
-    for (let i = 0; i < bodyData.length ; i += 1 ) {
-  try {
-      const items = bodyData[i];
-      await page.goto(bodyData[i].link, {
+  
+  for (let i = 0; i < bodyData.length; i += 1) {
+    try {
+      const currentItem = bodyData[i];
+      const imageSources = await page.goto(bodyData[i].link, {
         waitUntil: 'networkidle2',
-        timeout: 0,
-      });
-
-      const imageSources = await page.evaluate((items) => {
+        timeout: 10000, // 10초 후에 타임아웃
+      }).then(async () => {
         let pictureSelector;
-        if (items.link.includes("mercari")) {
-          // console.log(`Processing URL: ${items.link} for jp.mercari.com`);
+        if (currentItem.link.includes("mercari")) {
           pictureSelector = 'picture';
-        } else if (items.link.includes("amiami")) {
-          // console.log(`Processing URL: ${items.link} for www.amiami.jp`);
+        } else if (currentItem.link.includes("amiami")) {
           pictureSelector = '.main_image_area';
-        } else if (items.link.includes("rakuten")) {
-          // console.log(`Processing URL: ${items.link} for biccamera.rakuten.co.jp`);
+        } else if (currentItem.link.includes("rakuten")) {
           pictureSelector = '.popup-modal2';
-        } else if (items.link.includes("fril")) {
-          // console.log(`Processing URL: ${items.link} for item.fril.jp`);
+        } else if (currentItem.link.includes("fril")) {
           pictureSelector = '#photoFrame';
-        } else if (items.link.includes("paypayfleamarket")) {
+        } else if (currentItem.link.includes("paypayfleamarket")) {
           pictureSelector = '.slick-current';
-        } else {
-          // console.log(`No match found for URL: ${items.link}`);
         }
         const pictureElement = document.querySelector(pictureSelector);
 
@@ -38,33 +31,25 @@ const createCrawling = async (bodyData) => {
           const imgElement = pictureElement.querySelector('img');
           return imgElement ? imgElement.src : null;
         }
-      }, items);
-      console.log(imageSources);
+        return null;
+      });
 
-      if (imageSources !== null) {
-        imageData.push({
-          url: items.link,
-          imageSources: imageSources,
-        });
-      } else {
-        imageData.push({
-          url: bodyData[i].link,
-          imageSources: 'https://demofree.sirv.com/nope-not-here.jpg',
-        });
-      }
-    } catch {
+      imageData.push({
+        url: currentItem.link,
+        imageSources: imageSources ? imageSources : 'https://demofree.sirv.com/nope-not-here.jpg',
+      });
+    } catch (error) {
+      console.error(`Error processing URL: ${bodyData[i].link}`, error);
       imageData.push({
         url: bodyData[i].link,
         imageSources: 'https://demofree.sirv.com/nope-not-here.jpg',
       });
     }
-    }
-    
-    await browser.close();
-    return imageData;
+  }
+  
+  await browser.close();
+  return imageData;
 };
-
-//
 
 module.exports = {
   createCrawling,
