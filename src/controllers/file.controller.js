0ddc372b56359107614e8575b3fb15f83b6bba52
softforyou <softforyou247@gmail.com>
2024-02-25 20:38:53 +0900

    change-image-src

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -3,6 +3,7 @@ const ApiError = require('../utils/ApiError');
 const { errorData } = require('../utils/errorData');
 const { preSignS3Object, getObject } = require('../utils/upload');
 const catchAsync = require('../utils/catchAsync');
+const { Order } = require('../models');
 
 const file = catchAsync(async (req, res) => {
   if (req.file === undefined) {
@@ -23,6 +24,15 @@ const fileAsPublic = catchAsync(async (req, res) => {
     throw new ApiError(httpStatus.BAD_REQUEST, errorData.FILE_REQUIRED);
   }
   const tempUrl = getObject(req.file);
+  console.log(tempUrl, "'l!!?DF??")
+  let r = await Order.findOneAndUpdate(
+    { "_id": req.body.orderId }, // 찾고자 하는 문서의 조건
+    { $set: { [`items.${req.body.index}.imageSrc`]: tempUrl } }, // 업데이트할 내용
+    { new: true } // 업데이트된 문서를 반환하도록 설정
+  )
+  console.log(r,"rr")
+
+  // console.log(req.file, req.body.index, req.body.orderId, "보낸거", tempUrl);
   res.status(httpStatus.CREATED).send({
     originalName: req.file.originalname,
     key: req.file.key,
--- src/routes/v1/file.route.js
+++ src/routes/v1/file.route.js
@@ -4,7 +4,7 @@ const { upload, uploadAsPublic } = require('../../utils/upload');
 
 const router = express.Router();
 
-router.post('/', upload.single('file'), fileController.file);
+// router.post('/', upload.single('file'), fileController.file);
 router.post('/public', uploadAsPublic.single('file'), fileController.fileAsPublic);
 
 module.exports = router;
